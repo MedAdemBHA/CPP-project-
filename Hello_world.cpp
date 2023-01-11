2355e234262dc5288205d534145808a870468f0d
Mohamed Adem Bel Hadj Amor <80263820+MedAdemBHA@users.noreply.github.com>
2023-01-11 10:16:16 +0100

    first project using C++

---
+++ Hello_world.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+//The std is a short form of standard, the std namespace contains the built-in classes and declared functions
+using namespace std;
+int main(){
+
+cout<<"hello world";
+
+return 0;
+
+}
